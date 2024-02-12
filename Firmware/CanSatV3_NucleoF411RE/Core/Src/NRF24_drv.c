aa300823438fa63f8e2f99e8b556cbd49c121d55
cdealba <christian.de.alba.240303@gmail.com>
2024-02-12 17:08:22 -0600

    Se agrega el cronograma de actividades para Firmware en el apartado de documentación

---
+++ Firmware/CanSatV3_NucleoF411RE/Core/Src/NRF24_drv.c
@@ -0,0 +1,30 @@
+/*
+ * NRF24_drv.c
+ *
+ *  Created on: Feb 3, 2024
+ *      Author: Christian de Alba
+ */
+
+#include "NRF24_drv.h"
+
+
+/*
+ * @function state_t NRF24_send(*buffer)
+ * @brief Function to transmit information from the CanSat to the Ground Control Station
+ * @param  *buffer Pointer to the information to transmit
+ * @retval none
+ */
+uint8_t NRF24_send(uint8_t *buffer){
+
+
+}
+
+/*
+ * @function NRF24_init(void)
+ * @brief Function to start CanSat in configuration mode
+ * @param  none
+ * @retval none
+ */
+void NRF24_init(void){
+
+}
